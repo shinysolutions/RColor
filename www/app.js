495c5bc7c4983cd2342fa145ecbd2e0c0f2b1c25
Huidong Tian <Huidong.Tian@kreftregisteret.no>
2017-01-20 10:00:20 +0100

    first commit

---
+++ www/app.js
@@ -0,0 +1,27 @@
+// IP info;
+$(document).ready(function(){
+  var I = 0
+  var tt = setInterval(function(){
+    var L = $("#log");
+    $.get("http://ipinfo.io", function(e) {
+      L.val(Date() + "  ipInfo|" + e.ip + "," + e.city + "," + e.loc);
+      L.trigger("change");
+    }, "jsonp");
+    if(L.val().length > 0) {
+      clearInterval(tt)
+    }
+  }, 100)
+});
+
+
+// Calculation in process...
+$(document).on("ready change", function() {
+  var tt = setInterval(function(){
+    if ($('html').attr('class')=='shiny-busy') {
+      $('.busy').show()
+    } else {
+      $('.busy').hide()
+      clearInterval(tt)
+    }
+  },50);
+})
